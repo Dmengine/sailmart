a46283b43b185963a9d7b1d86ffcfa1aa8021afe
yusfit <princefesomuyusuf@gmail.com>
2025-01-26 16:53:20 +0100

    updated

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -110,7 +110,7 @@ const ProductList = () => {
               </svg>
             </div>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
+              {product.rating.rate}
             </span>
           </div>
           <div className="flex items-center justify-between">
