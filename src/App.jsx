47e7c5fe96f40377c264feacb40121918c5c4c6c
Aminat Amusa <amusaaminat069@gmail.com>
2025-01-26 12:16:40 +0100

    About page updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Cart from './pages/cart/Cart';
 import ProductList from './pages/productList/ProductList';
+import About from './pages/about/About';
 
 const App = () => {
   return (
     <Router>
       <nav className="flex flex-col">
-        <Link to="/about">About</Link>
         <Link to="/">Product</Link>
         <Link to="/cart">Cart</Link>
+        <Link to="/about">About</Link>
       </nav>
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<Cart/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </Router>
   )
