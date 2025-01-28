248ec1fa1bc69a60714443d74a35d1475bfe32f4
etukudoemem <saspiee@gmail.com>
2025-01-28 22:54:59 +0100

    fixed placeholder bug

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react"
 import phoneImgUrl from "../../assets/phone-icon.png"
 import emailImgUrl from "../../assets/email-icon.png"
 
 export default function Contact() {
 
+    const [ value, setValue ] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    })
+
+    function handleInput (e) {
+        const { name, value } = e.target 
+        setValue({...value, [name]: value})
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        console.log("submitted")
+    }
+
     return (
         <>
-            <h4 className="px-36 my-8 hidden lg:block"><span className="opacity-50" >Home /</span><span> Contact</span></h4>
-            <main className="flex justify-center items-center lg:flex-row sm:mt-0 mt-16">
+            <h4 className="px-36 my-8 hidden sm:block"><span className="opacity-50" >Home /</span><span> Contact</span></h4>
+            <main className="flex justify-center items-center sm:flex-row sm:mt-0 mt-16 px-4">
                 <div className="flex flex-col gap-y-4 sm:flex-row sm:gap-8 justify-center
                     items-center">
-                    <section className="flex flex-col w-[100%] h-[auto] lg:w-[50%] lg:h-[457px] 
-                        sm:py-10 sm:px-9 py-8 px-9 rounded sm:shadow-lg mb-0 justify-center
+                    <section className="flex flex-col w-[100%] h-[auto] sm:w-[50%] sm:h-[457px] 
+                        sm:py-10 sm:px-9 py-8 px-9 rounded sm:shadow-sm mb-0 justify-center
                         border-gray-300 border-solid sm:border-2 sm:mb-0 text-xl sm:text-base">
                         <div className="flex items-center space-x-2
                             mb-4">
@@ -39,47 +57,45 @@ export default function Contact() {
                         </p>
                         <div className="max-w-full md:max-w-full h-[1px] bg-gray-400
                             opacity-50 mt-8 sm:hidden"></div>
-                        
                     </section>
                     <section className="w-[100%] h-[auto] sm:h-[457px] sm:py-14 sm:px-9 
-                        pb-4 px-3 rounded sm:shadow-lg overflow-hidden
+                        pb-4 px-3 rounded sm:shadow-sm overflow-hidden
                         border-gray-300 border-solid sm:border-2 text-xl sm:text-base">
                         <h1 className="font-semibold text-2xl text-center mb-8
                             sm:hidden">
                             Send Us a Message
                         </h1>
-                        <form>
-                            <div className="grid grid-cols-3 grid-rows-5 gap-x-4 gap-y-6">
+                        <form onSubmit={handleSubmit}>
+                            <div className="grid grid-cols-3 grid-rows-5 gap-x-4 gap-y-6 relative">
                                 <input className="sm:col-span-1 sm:row-span-1 col-span-3 
                                     bg-gray-200 py-3 px-6 sm:px-4 opacity-50 sm:outline-none
                                     cursor-pointer outline-black" 
-                                    placeholder="Your Name" 
+                                    placeholder="Your Name" onChange={handleInput} 
                                     id="name" name="name" type="text" required
                                 />
                                 <input className="sm:col-span-1 sm:row-span-1 col-span-3 
                                     bg-gray-200 py-3 px-6 sm:px-4 opacity-50 sm:outline-none
                                     cursor-pointer outline-black" 
-                                    placeholder="Your Name" 
-                                    id="name" name="name" type="text" required
+                                    placeholder="Your Email" onChange={handleInput} 
+                                    id="email" name="email" type="email" required 
                                 />
                                 <input className="sm:col-span-1 sm:row-span-1 col-span-3 
                                     bg-gray-200 py-3 px-6 sm:px-4 opacity-50 sm:outline-none
                                     cursor-pointer outline-black" 
-                                    placeholder="Your Name" 
-                                    id="name" name="name" type="text" required
+                                    placeholder="Your Phone Number" onChange={handleInput}
+                                    id="phone" name="phone" type="number" required
                                 />
                                 <textarea className="col-span-3 row-span-3 bg-gray-200 
                                     py-3 px-6 resize-none sm:px-4 opacity-50 sm:outline-none
                                     cursor-pointer outline-black" 
-                                    placeholder="Your Message"
+                                    placeholder="Your Message" onChange={handleInput}
                                     id="text" name="message" maxLength={750}>
                                 </textarea>
                                 <button className="sm:col-start-3 sm:col-end-4 col-span-3 
                                     bg-[#db4444] text-[#fafafa] rounded py-3 px-6
                                     sm:justify-self-end cursor-pointer sm:w-[215px]" 
                                     type="submit">Send Message
-                                </button>
-                                
+                                </button> 
                             </div>
                         </form>
                     </section>
