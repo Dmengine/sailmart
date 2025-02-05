938c10999adfc4a39b1662a4bdb22bd7177e3516
Oladimeji <azeezoladimeji23@gmail.com>
2025-02-05 21:20:58 +0100

    About page images fixed

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
           </p>
         </div>
         <img
-          src="/public/about_img.png"
+          src="/about_img.png"
           alt=""
           className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[45%] max-w-full"
         />
@@ -31,28 +31,28 @@ const About = () => {
 
       <div className="flex flex-wrap justify-center gap-5 lg:gap-10 mt-25">
         <div className="border border-gray-200 shadow-md flex flex-col items-center w-70 h-58 mt-10 p-10 text-base leading-10 transition-transform duration-300 hover:bg-red-600 hover:shadow-lg">
-          <img src="/public/Services1.png" alt="" className="max-w-20" />
+          <img src="/Services1.png" alt="" className="max-w-20" />
           <h1 className="font-sans text-2xl font-semibold pt-5 tracking-[.05em]">
             10.5k{" "}
           </h1>
           <p>Sallers active our site</p>
         </div>
         <div className="border border-gray-200 shadow-md flex flex-col items-center w-70 h-58 mt-10 p-10 text-base leading-10 transition-transform duration-300 hover:bg-red-600 hover:shadow-lg">
-          <img src="/public/services4.png" alt="" className="max-w-20" />
+          <img src="/services4.png" alt="" className="max-w-20" />
           <h1 className="font-sans text-2xl font-semibold pt-5 tracking-[.05em]">
             33k{" "}
           </h1>
           <p>Monthly Product Sale</p>
         </div>
         <div className="border border-gray-200 shadow-md flex flex-col items-center w-70 h-58 mt-10 p-10 text-base leading-10 transition-transform duration-300 hover:bg-red-600 hover:shadow-lg">
-          <img src="/public/services2.png" alt="" className="max-w-20" />
+          <img src="/services2.png" alt="" className="max-w-20" />
           <h1 className="font-sans text-2xl font-semibold pt-5 tracking-[.05em]">
             45.5k{" "}
           </h1>
           <p>Customer active in our site</p>
         </div>
         <div className="border border-gray-200 shadow-md flex flex-col items-center w-70 h-58 mt-10 p-10 text-base leading-10 transition-transform duration-300 hover:bg-red-600 hover:shadow-lg">
-          <img src="/public/services3.png" alt="" className="max-w-20" />
+          <img src="/services3.png" alt="" className="max-w-20" />
           <h1 className="font-sans text-2xl font-semibold pt-5 tracking-[.05em]">
             25k{" "}
           </h1>
@@ -62,7 +62,7 @@ const About = () => {
 
       <div className="flex flex-wrap justify-center gap-5 lg:gap-10 mt-60">
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col gap-5">
-          <img src="/public/person1.png" alt="" className="max-w-82" />
+          <img src="/person1.png" alt="" className="max-w-82" />
           <div>
             <h1 className="font-sans text-2xl font-semibold tracking-[.05em]">
               Will Smith
@@ -76,7 +76,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col gap-5">
-          <img src="/public/person2.png" alt="" className="max-w-82" />
+          <img src="/person2.png" alt="" className="max-w-82" />
           <div>
             <h1 className="font-sans text-2xl font-semibold tracking-[.05em]">
               Emma Watson
@@ -90,7 +90,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col gap-5">
-          <img src="/public/person3.png" alt="" className="max-w-82" />
+          <img src="/person3.png" alt="" className="max-w-82" />
           <div>
             <h1 className="font-sans text-2xl font-semibold tracking-[.05em]">
               Tom Cruise
@@ -107,7 +107,7 @@ const About = () => {
 
       <div className="flex flex-wrap justify-center items-center gap-5 lg:gap-10 mt-5">
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col items-center gap-5">
-          <img src="/public/services5.png" alt="" className="max-w-20" />
+          <img src="/services5.png" alt="" className="max-w-20" />
           <div className="text-center">
             <h1 className="font-sans text-lg font-semibold tracking-tight">
               FREE AND FAST DELIVERY
@@ -116,7 +116,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col items-center gap-5">
-          <img src="/public/services6.png" alt="" className="max-w-20" />
+          <img src="/services6.png" alt="" className="max-w-20" />
           <div className="text-center">
             <h1 className="font-sans text-lg font-semibold tracking-tight">
               24/7 CUSTOMER SERVICE
@@ -125,7 +125,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-4 md:px-8 py-4 sm:py-10 flex flex-col items-center gap-5">
-          <img src="/public/services7.png" alt="" className="max-w-20" />
+          <img src="/services7.png" alt="" className="max-w-20" />
           <div className="text-center">
             <h1 className="font-sans text-lg font-semibold tracking-tight">
               MONEY BACK GUARANTEE
